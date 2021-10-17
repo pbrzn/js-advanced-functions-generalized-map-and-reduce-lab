0513a930c01064635cbdbc3736dd902383dafbf3
Patrick Brennan <pbrennanmusic@gmail.com>
2021-10-16 23:04:23 -0400

    commit tree 5af652a724a0c27ce9bdc33930f16ee6f9f25519

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your functions here
+function map(array, callback) {
+  const result = [];
+  for (const element of array) {
+    let newElement = callback(element);
+    result.push(newElement);
+  }
+  return result;
+}
+
+//This took me a while to figure out. Adding comments for notes later...
+function reduce(array, callback, startingValue) {
+//If a startingValue is present, "memo" will be equal that...
+//...otherwise it will equal the first element of array...
+  let memo = (!!startingValue) ? startingValue : array[0];
+//"i" is defined here so that when we enter the loop...
+//...we start from array[0] if startingValue is present...
+//...and array[1] if not.
+
+  let i = (!!startingValue) ? 0 : 1;
+//This is all done to prevent the function calls that evaluate...
+//...truthiness of all values from catching a false startingValue...
+//...which would occur if we were to set it to a default value of 0.
+
+  for (; i < array.length; i++) {
+    memo = callback(array[i], memo)
+  }
+  return memo;
+}
